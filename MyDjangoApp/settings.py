c9e80601265a400b9a35d956c20b9d1965b931d2
vonsalumbides <salumbides.von@gmail.com>
2020-08-04 11:27:12 +0800

    Django app

--- MyDjangoApp/settings.py
+++ MyDjangoApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l30v67xdqb$0hjr6e15+yvul$9hnfaz)ze&(*bfe$)(-!u_+-m'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
